f7e5df445260235957057cb3cbec275b1cf74646
Yusuf Hamza Çelebi <yhcelebi@gmail.com>
2023-07-19 10:55:14 +0300

    deployment

--- muvime/src/index.tsx
+++ muvime/src/index.tsx
@@ -4,14 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
