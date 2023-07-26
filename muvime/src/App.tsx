a3579f1fcc7fe31fca945764deebe2417a5b2a6b
Yusuf Hamza Çelebi <yhcelebi@gmail.com>
2023-07-26 14:36:02 +0300

    formatted the files and deleted the scss files that doesnt function

--- muvime/src/App.tsx
+++ muvime/src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
 import { Header } from './components/Header/Header';
 import { Introduction } from './components/Introduction/Introduction';
-import { Card } from './components/Card/Card';
 import { Footer } from './components/Footer/Footer';
-import { MovieCard } from './components/Card/components/MovieCard/MovieCard';
-import { Legacy } from './components/Legacy/Legacy';
 import { Slider } from './components/Slider/Slider';
 
 function App() {
